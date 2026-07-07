557ea29ce492473cbb770f8f5ac11222e33faf21
leidylys <leidy.lys@gmail.com>
2026-07-07 10:10:57 +0000

    fix: usar puerto dinamico para despliegue

--- server.ts
+++ server.ts
@@ -78,7 +78,7 @@ function maskDatabaseUrl(rawUrl?: string) {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   // JSON Body Parser
   app.use(express.json({ limit: '10mb' }));
